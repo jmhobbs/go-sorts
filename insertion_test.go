4a4169a0cb5f3812d2bbdfb5b06b5844766ebfb0
John Hobbs <john@velvetcache.org>
2019-10-14 15:54:33 -0500

    Testing helper.

--- insertion_test.go
+++ insertion_test.go
@@ -1,17 +1,9 @@
 package sorts
 
 import (
-	"reflect"
 	"testing"
 )
 
 func TestInsertionSort(t *testing.T) {
-	input := []int{5, 1, 4, 2, 8}
-	expected := []int{1, 2, 4, 5, 8}
-
-	actual := InsertionSort().Sort(input)
-
-	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("bad sort.\nexpected: %v\n  actual: %v", expected, actual)
-	}
+	testSamples("Insertion Sort", InsertionSort(), t)
 }
